8db600367ab454cb42454caa5a652c0325e304d6
Anwesh43 <anweshthecool0@gmail.com>
2018-03-31 03:07:58 +0530

    adding PlugView class

--- plugview/src/main/java/ui/anwesome/com/plugview/PlugView.kt
+++ plugview/src/main/java/ui/anwesome/com/plugview/PlugView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.plugview
 /**
  * Created by anweshmishra on 31/03/18.
  */
+import android.content.Context
+import android.graphics.*
+import android.view.*
+class PlugView (ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw (canvas : Canvas) {
+
+    }
+    override fun onTouchEvent (event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
