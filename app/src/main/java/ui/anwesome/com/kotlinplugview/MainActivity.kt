77b6ab20e16350ed9acd2b22f7c9171560cc30c1
Anwesh43 <anweshthecool0@gmail.com>
2018-03-31 03:42:10 +0530

    adding view in activity

--- app/src/main/java/ui/anwesome/com/kotlinplugview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinplugview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinplugview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.plugview.PlugView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PlugView.create(this)
     }
 }
